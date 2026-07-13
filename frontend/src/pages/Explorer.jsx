13aa53791a55fc7b03c75c187a89711b8c9174d5
Trupthi KB <trupthikb23@gmail.com>
2026-07-13 19:59:55 +0530

    Add live planet search

--- frontend/src/pages/Explorer.jsx
+++ frontend/src/pages/Explorer.jsx
@@ -1,52 +1,132 @@
 import { useEffect, useState } from "react";
-import { TextField, Typography } from "@mui/material";
+import {
+  TextField,
+  Typography,
+  MenuItem,
+  Grid,
+  CircularProgress,
+  Box,
+} from "@mui/material";
 
 import api from "../services/api";
 import PlanetCard from "../components/Planet/PlanetCard";
 
 function Explorer() {
+  const [planets, setPlanets] = useState([]);
 
-    const [planets, setPlanets] = useState([]);
+  const [search, setSearch] = useState("");
 
-    useEffect(() => {
+  const [method, setMethod] = useState("");
 
-        api.get("/planets/search")
-            .then((res) => {
-                setPlanets(res.data);
-            });
+  const [year, setYear] = useState("");
 
-    }, []);
+  const [loading, setLoading] = useState(false);
 
-    return (
+  useEffect(() => {
+    setLoading(true);
 
-        <>
+    api
+      .get(
+        `/planets/search?name=${search}&method=${method}&year=${year}`
+      )
+      .then((res) => {
+        setPlanets(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [search, method, year]);
 
-            <Typography
-                variant="h4"
-                gutterBottom
-            >
-                Planet Explorer
-            </Typography>
+  return (
+    <Box>
 
-            <TextField
-                fullWidth
-                placeholder="Search planets..."
-                sx={{ mb: 4 }}
-            />
+      <Typography
+        variant="h4"
+        gutterBottom
+      >
+        Planet Explorer
+      </Typography>
 
-            {planets.map((planet) => (
+      {/* Search Box */}
 
-                <PlanetCard
-                    key={planet.id}
-                    planet={planet}
-                />
+      <TextField
+        fullWidth
+        placeholder="Search planets..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        sx={{ mb: 3 }}
+      />
 
-            ))}
+      {/* Filters */}
 
-        </>
+      <Grid
+        container
+        spacing={2}
+        sx={{ mb: 4 }}
+      >
+        <Grid item xs={12} md={6}>
+          <TextField
+            select
+            fullWidth
+            label="Discovery Method"
+            value={method}
+            onChange={(e) => setMethod(e.target.value)}
+          >
+            <MenuItem value="">All</MenuItem>
+            <MenuItem value="Transit">Transit</MenuItem>
+            <MenuItem value="Radial Velocity">
+              Radial Velocity
+            </MenuItem>
+            <MenuItem value="Imaging">
+              Imaging
+            </MenuItem>
+            <MenuItem value="Microlensing">
+              Microlensing
+            </MenuItem>
+          </TextField>
+        </Grid>
 
-    );
+        <Grid item xs={12} md={6}>
+          <TextField
+            fullWidth
+            type="number"
+            label="Discovery Year"
+            value={year}
+            onChange={(e) => setYear(e.target.value)}
+          />
+        </Grid>
+      </Grid>
 
+      {/* Loading */}
+
+      {loading && (
+        <Box sx={{ textAlign: "center", mt: 4 }}>
+          <CircularProgress />
+        </Box>
+      )}
+
+      {/* No Results */}
+
+      {!loading && planets.length === 0 && (
+        <Typography align="center">
+          No planets found.
+        </Typography>
+      )}
+
+      {/* Planet Cards */}
+
+      {!loading &&
+        planets.map((planet) => (
+          <PlanetCard
+            key={planet.id}
+            planet={planet}
+          />
+        ))}
+    </Box>
+  );
 }
 
 export default Explorer;
